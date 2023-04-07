a4e5fa22c12e75949d30e4096adc424ce152c388
Steven <liang1058@126.com>
2023-04-07 17:19:58 +0800

    update

--- cpp_template/trie.cpp
+++ cpp_template/trie.cpp
@@ -48,9 +48,10 @@ private:
     int L;
     Trie* left;
     Trie* right;
+    long long cnt;
 
 public:
-    Trie() : L(31), left(nullptr), right(nullptr) {}
+    Trie() : L(31), left(nullptr), right(nullptr), cnt(0) {}
 
     void insert(long long val) {
         Trie* node = this;
@@ -66,6 +67,7 @@ public:
                 }
                 node = node->right;
             }
+            node->cnt++;
         }
     }
 
@@ -93,5 +95,43 @@ public:
             }
         }
         return ans;
+    }
+
+    long long query_cnt(long long val, long long limit) {
+        // 严格小于limit的数对异或值数量
+        Trie* node = this;
+        long long ans = 0;
+        for (int i = L; i >= 0; i--) {
+            if (node == nullptr) {
+                break;
+            }
+            auto cur = val >> i & 1;
+            if (limit >> i & 1) {
+                if (!cur && node->left != nullptr) {
+                    ans += node->left->cnt;
+                }
+                if (cur && node->right != nullptr) {
+                    ans += node->right->cnt;
+                }
+                if (cur) node = node->left;
+                else node = node->right;
+            } else {
+                if (!cur) node = node->left;
+                else node = node->right;
+            }
+        }
+        return ans;
+    }
+
+    void remove(long long val) {
+        Trie* node = this;
+        for (int i = L; i >= 0; i--) {
+            if (val >> i & 1) {
+                node = node->right;
+            } else {
+                node = node->left;
+            }
+            node->cnt--;
+        }
     }
 };
