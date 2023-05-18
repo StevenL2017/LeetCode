30bdd2b1191540d8c1c1ecbf04a91b3a65752763
Steven <liang1058@126.com>
2023-05-18 14:20:45 +0800

    update

--- cpp_template/tarjan.cpp
+++ cpp_template/tarjan.cpp
@@ -59,3 +59,35 @@ void tarjan_node(int u, int p) {
         }
     }
 }
+
+const int N = 1e5 + 3;
+
+int dfn[N], low[N], dfncnt, s[N], in_stack[N], tp;
+int scc[N], sc;  // 结点 i 所在 SCC 的编号
+int sz[N];       // 强连通 i 的大小
+
+void tarjan_scc(int u) {
+    if (dfn[u]) return;
+    low[u] = dfn[u] = ++dfncnt, s[++tp] = u, in_stack[u] = 1;
+    for (auto v : e[u]) {
+        if (!dfn[v]) {
+            tarjan_scc(v);
+            low[u] = min(low[u], low[v]);
+        } else if (in_stack[v]) {
+            low[u] = min(low[u], dfn[v]);
+        }
+    }
+    if (dfn[u] == low[u]) {
+        ++sc;
+        while (s[tp] != u) {
+            scc[s[tp]] = sc;
+            sz[sc]++;
+            in_stack[s[tp]] = 0;
+            --tp;
+        }
+        scc[s[tp]] = sc;
+        sz[sc]++;
+        in_stack[s[tp]] = 0;
+        --tp;
+    }
+}
