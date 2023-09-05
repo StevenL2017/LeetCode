8ba4094b963c76eb02a28da748a6a53b8ec89591
StevenL2017 <liang1058@126.com>
2023-09-05 23:29:22 +0800

    update

--- python_template/binary_search.py
+++ python_template/binary_search.py
@@ -1,3 +1,5 @@
+from typing import *
+
 # 左闭右闭
 def binary_search_1():
     def check():
@@ -27,3 +29,19 @@ def binary_search_2():
         else:
             right = mid
     return left
+
+# 三分法求严格凸函数 fn 在 [lower, upper] 闭区间内的最小值
+def minimize(fn: Callable, lower: int, upper: int) -> int:
+    ans = float('inf')
+    while upper - lower >= 3:
+        diff = upper - lower
+        mid1 = lower + diff // 3
+        mid2 = lower + 2 * diff // 3
+        if fn(mid1) > fn(mid2):
+            lower = mid1
+        else:
+            upper = mid2
+    while lower <= upper:
+        ans = min(ans, fn(lower))
+        lower += 1
+    return ans
--- python_template/lca.py
+++ python_template/lca.py
@@ -27,3 +27,40 @@ def lca(root: TreeNode, nodes: List[TreeNode]) -> TreeNode:
     needs = set(nodes)
     subtreeSum = defaultdict(int)
     return next(dfs(root))
+
+# 倍增法求LCA
+n = 10 ** 5
+e = [[] for _ in range(n)]
+depth = [0] * n
+f = [[0] * 31 for _ in range(n)]
+def dfs(u: int, p: int):
+    if p != -1:
+        depth[u] = depth[p] + 1
+        f[u][0] = p
+    for i in range(1, 31):
+        f[u][i] = f[f[u][i - 1]][i - 1]
+    for v, w in e[u]:
+        if v == p:
+            continue
+        dfs(v, u)
+dfs(0, -1)
+
+def lca(x: int, y: int) -> int:
+    if depth[x] > depth[y]:
+        x, y = y, x
+    temp = depth[y] - depth[x]
+    i = 0
+    while temp:
+        if temp & 1:
+            y = f[y][i]
+        i += 1
+        temp >>= 1
+    if x == y:
+        return y
+    for j in range(30, -1, -1):
+        if y == x:
+            break
+        if f[x][j] != f[y][j]:
+            x = f[x][j]
+            y = f[y][j]
+    return y if y == x else f[y][0]
